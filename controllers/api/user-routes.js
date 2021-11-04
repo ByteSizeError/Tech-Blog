4a6b7fd5703f7053be2951d6f791242224abf3ea
Jimmy Zheng <jimmyz0622@gmail.com>
2021-11-04 15:45:46 -0700

    Update create user route to / from /signup

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
+router.get("/", async (req, res) => {
+    try {
+        const userData = await User.findAll({});
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 router.post("/", async (req, res) => {
     try {
         const userData = await User.create(req.body);
@@ -19,7 +28,7 @@ router.post("/", async (req, res) => {
 router.post("/login", async (req, res) => {
     try {
         const userData = await User.findOne({
-            where: { username: req.body.username },
+            where: { name: req.body.username },
         });
 
         if (!userData) {
