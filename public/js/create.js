33dc605ba93e1d83c5f2091363d1101563904fe7
Jimmy Zheng <jimmyz0622@gmail.com>
2021-11-05 21:49:01 -0700

    Add create post route can now add posts to db

---
+++ public/js/create.js
@@ -0,0 +1,23 @@
+const createFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector("#title-create").value.trim();
+    const content = document.querySelector("#content-create").value.trim();
+    if (title && content) {
+        const response = await fetch("/api/posts/", {
+            method: "POST",
+            body: JSON.stringify({ title, content }),
+            headers: { "Content-Type": "application/json" },
+        });
+
+        if (response.ok) {
+            document.location.replace("/dashboard");
+        } else {
+            alert("Failed to create post");
+        }
+    }
+};
+
+document
+    .querySelector(".create-form")
+    .addEventListener("submit", createFormHandler);
