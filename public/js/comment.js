c742e17346402022b6a37edc06817711fefc4d3e
Jimmy Zheng <jimmyz0622@gmail.com>
2021-11-07 20:48:51 -0800

    Add comment handlers for update and delete

--- public/js/comment.js
+++ public/js/comment.js
@@ -20,6 +20,54 @@ const commentFormHandler = async (event) => {
     }
 };
 
+const updateCommentHandler = async (event) => {
+    event.preventDefault();
+
+    const postId = window.location.href.split("/").pop();
+
+    const id = event.target.id.split("-").pop();
+
+    const content = document.querySelector(`#content-${id}`).value.trim();
+
+    if (content) {
+        const response = await fetch(`/api/comments/${id}`, {
+            method: "PUT",
+            body: JSON.stringify({ content }),
+            headers: { "Content-Type": "application/json" },
+        });
+
+        if (response.ok) {
+            document.location.replace(`/posts/${postId}`);
+        } else {
+            alert("Failed to update comment");
+        }
+    }
+};
+
+const deleteCommentHandler = async (event) => {
+    event.preventDefault();
+
+    const postId = window.location.href.split("/").pop();
+
+    const id = event.target.id.split("-").pop();
+
+    const response = await fetch(`/api/comments/${id}`, {
+        method: "DELETE",
+    });
+
+    if (response.ok) {
+        document.location.replace(`/posts/${postId}`);
+    } else {
+        alert("Failed to delete comment");
+    }
+};
+
 document
     .querySelector(".comment-form")
     .addEventListener("submit", commentFormHandler);
+
+const updates = document.querySelectorAll(".update-comment");
+updates.forEach((el) => el.addEventListener("click", updateCommentHandler));
+
+const deletes = document.querySelectorAll(".delete-comment");
+deletes.forEach((el) => el.addEventListener("click", deleteCommentHandler));
